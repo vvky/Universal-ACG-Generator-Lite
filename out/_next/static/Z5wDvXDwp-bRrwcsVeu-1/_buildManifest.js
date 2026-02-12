ab9812c3995225c85b2865a4bdf35723288d0ed0
vvky <vvkyjay@gmail.com>
2026-02-13 02:08:41 +0800

    what can i say

---
+++ out/_next/static/Z5wDvXDwp-bRrwcsVeu-1/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Universal-ACG-Generator-Lite//_next/:path+",
+        "destination": "/Universal-ACG-Generator-Lite/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
